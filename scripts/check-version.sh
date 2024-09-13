a6a59b0de8c57fc459f67f968fb66516a94fb497
Yoshitaka motomura <stay.daybreak@gmail.com>
2024-09-14 02:40:09 +0900

    Refactor GitHub Actions workflows and Dockerfile

---
+++ scripts/check-version.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+## 引数を受け取る
+VERSON=$1
+
+RESPONSE=$(curl -s "https://www.php.net/releases/index.php?json&version={$VERSON}" | jq ".version")
+
+## バージョンが存在しない場合
+if [ "$RESPONSE" = "null" ]; then
+  echo "バージョンが存在しません"
+  exit 1
+else
+    echo "バージョンが存在します"
+    exit 0
+fi
